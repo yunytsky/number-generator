fc11f71bb7ef9b1920cce4b7f604cd8dba85d137
Cy <105945635+cyber8nav@users.noreply.github.com>
2023-01-30 11:15:14 +0300

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const Main = () => {
+
+   const [min, setMin] = useState(0);
+   const [max, setMax] = useState(100);
+   const [number, setNumber] = useState(Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min)));
+   const [error, setError] = useState("")
+
+   function generate(e) {
+      e.preventDefault();
+      if(min > max) {
+         const minNumber = min;
+         setMin(max);
+         setMax(min);
+      }
+      setNumber(Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min)));
+   }
+
+   return (
+      <div className="main">
+         <span className="main__header">Number generator</span>
+         {error && <span className="error">{error}</span>}
+         <form>
+            <div className="main__inputs">
+               <label htmlFor="minimum">
+                  Min
+                  <input type="number" id="minimum" name="minimum" value={min} onChange={(e) => {
+                     setMin(e.target.value)
+                     if (isNaN(parseInt(e.target.value))) {
+                        setError("Type a number")
+                     }else if (!isNaN(parseInt(e.target.value))) {
+                        setError()
+                     }
+                  } } />
+               </label>
+
+               <label htmlFor="maximum" >
+                  Max
+                  <input type="number" id="maximum" name="maximum" value={max} onChange={(e) => {
+                     setMax(e.target.value)
+                     if (isNaN(parseInt(e.target.value)) && e.target.value != "") {
+                        setError("Type a number")
+                     } else if (!isNaN(parseInt(e.target.value))) {
+                        setError()
+                     }
+                  }} />
+               </label>
+            </div>
+
+            
+
+            <button className="main__button" onClick={generate}>Generate</button>
+         </form>
+         <div className="main__output">{number}</div>
+      </div>
+   );
+}
+ 
+export default Main;
